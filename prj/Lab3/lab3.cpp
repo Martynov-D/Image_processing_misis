34403e9c53ecdda3b06acedc396ce157630609a5
Martynov Dima <dimamartynov97@mail.ru>
2020-05-08 02:58:59 +0300

    added pdf reports

--- prj/Lab3/lab3.cpp
+++ prj/Lab3/lab3.cpp
@@ -3,60 +3,13 @@
 #include <vector>
 
 
-void first()
-{
-	IplImage* image = cvLoadImage("C:/Users/dimam/Documents/Admin/Programming/C++/OpenCV/OpenCVData/test_data/parrot.jpg", 0);
-
-	int hist_size = 256;
-	float range[] = { 0, 256 };
-	float* histRange[] = { range };
-
-	CvHistogram* hist;
-	IplImage* hist_image = cvCreateImage(cvSize(1024, 720), 8, 1);
-	hist = cvCreateHist(1, &hist_size, CV_HIST_ARRAY, histRange, 1);
-	cvCalcHist(&image, hist, 0, NULL);
-
-	int bin_w;
-	bin_w = cvRound((double)hist_image->width / hist_size);
-
-	cvSet(hist_image, cvScalarAll(255), 0);
-	for (int i{ 0 }; i < hist_size; ++i)
-		cvRectangle
-		(
-			hist_image,
-			cvPoint(i * bin_w, hist_image->height),
-			cvPoint((i + 1) * bin_w, hist_image->height - cvRound(cvGetReal1D(hist->bins, i))),
-			cvScalarAll(0),
-			-1,
-			8,
-			0
-		);
-
-	// показываем результат
-	cvNamedWindow("Gray", 1);
-	cvShowImage("Gray", image);
-
-	cvNamedWindow("Histogram", 1);
-	cvShowImage("Histogram", hist_image);
-
-	// ждём нажатия клавиши
-	cvWaitKey(0);
-
-	// освобождаем ресурсы
-	cvReleaseImage(&image);
-	cvReleaseImage(&hist_image);
-
-	// удаляем окна
-	cvDestroyAllWindows();
-}
-
-void second(cv::Mat image)
+void grayAndColorHist(cv::Mat image)
 {
 	// Преобразование в изображение в оттенках серого
 	cv::Mat imageGray;
 	cv::cvtColor(image, imageGray, CV_BGR2GRAY);
 
-	// Вектор, который хранит монохроматичную картинку
+	// Вектор, который хранит разложение на каналы
 	std::vector<cv::Mat> bgrPlanes;
 	cv::split(image, bgrPlanes);
 
@@ -80,14 +33,14 @@ void second(cv::Mat image)
 
 	calcHist(&imageGray, 1, 0, cv::Mat(), gray_hist, 1, &histSize, &histRange, uniform, accumulate);
 
-	int hist_w{ 1280 }; // 512
-	int hist_h{ 720 }; // 400
+	int hist_w{ 512 }; // 512
+	int hist_h{ 400 }; // 400
 	int bin_w{ cvRound((double)hist_w / histSize) };
 
 	// Создание пустых изображений для гистограмм
 	cv::Mat histImage(hist_h, hist_w, CV_8UC3, cv::Scalar(0, 0, 0));
 
-	cv::Mat grayHistImage(hist_h, hist_w, CV_8UC3, cv::Scalar(255, 255, 255));
+	cv::Mat grayHistImage(hist_h, hist_w, CV_8UC1, 255);
 
 	// Нормализация гистограмм под размер окна
 	cv::normalize(b_hist, b_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
@@ -130,15 +83,81 @@ void second(cv::Mat image)
 	cv::waitKey(0);
 }
 
+void lab2()
+{
+	cv::Mat image = cv::imread("C:/Admin/Programming/C++/OpenCV/data/test_data/parrot.jpg", CV_LOAD_IMAGE_COLOR);
+
+	// Повышение яркости изображения
+	cv::Mat imageB = image + cv::Scalar(30, 30, 30);
+
+	// Преобразование в изображение в оттенках серого
+	cv::Mat imageGray;
+	cv::cvtColor(image, imageGray, CV_BGR2GRAY);
+
+	cv::Mat imageGrayB;
+	cv::cvtColor(imageB, imageGrayB, CV_BGR2GRAY);
+
+	int histSize{ 256 };
+	float range[] = { 0,256 };
+	const float* histRange = { range };
+
+	bool uniform{ true };
+	bool accumulate{ false };
+
+	int histWidth{ 512 }; // 512
+	int histHeight{ 400 }; // 400
+	int bin_w{ cvRound((double)histWidth / histSize) };
+
+	// Создание пустых изображений для гистограмм
+	cv::Mat hist_window(histHeight, histWidth, CV_8UC1, 255); // cv::Scalar(255, 255, 255)
+	cv::Mat histBright_window(histHeight, histWidth, CV_8UC1, 255);
+
+	// Рассчет гистограмм
+	cv::Mat hist;
+	cv::Mat histBright;
+	calcHist(&imageGray, 1, 0, cv::Mat(), hist, 1, &histSize, &histRange, uniform, accumulate);
+	calcHist(&imageGrayB, 1, 0, cv::Mat(), histBright, 1, &histSize, &histRange, uniform, accumulate);
+
+	// Нормализация гистограмм под размер окна
+	cv::normalize(hist, hist, 0, hist_window.rows, cv::NORM_MINMAX, -1, cv::Mat());
+	cv::normalize(histBright, histBright, 0, histBright_window.rows, cv::NORM_MINMAX, -1, cv::Mat());
+
+	// Построение гистограмм
+	for (int i = 1; i < histSize; ++i)
+	{
+		line(hist_window, cv::Point(bin_w * (i - 1), histHeight - cvRound(hist.at<float>(i - 1))),
+			cv::Point(bin_w * (i), histHeight - cvRound(hist.at<float>(i))),
+			cv::Scalar(0, 0, 0), 2, 8, 0);
+		line(histBright_window, cv::Point(bin_w * (i - 1), histHeight - cvRound(histBright.at<float>(i - 1))),
+			cv::Point(bin_w * (i), histHeight - cvRound(histBright.at<float>(i))),
+			cv::Scalar(0, 0, 0), 2, 8, 0);
+	}
+
+	// Отображение результатов
+	cv::namedWindow("Original image in gray", CV_WINDOW_AUTOSIZE);
+	cv::namedWindow("Hist", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Bright image", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Bright Hist", cv::WINDOW_AUTOSIZE);
+
+	cv::imshow("Original image in gray", imageGray);
+	cv::imshow("Hist", hist_window);
+	cv::imshow("Bright image", imageGrayB);
+	cv::imshow("Bright Hist", histBright_window);
+
+	cv::waitKey(0);
+
+	cv::destroyAllWindows();
+}
+
 int main()
 {
 	// Загрузка изображения
-	cv::Mat image{ cv::imread("C:/Users/dimam/Documents/Admin/Programming/C++/OpenCV/OpenCVData/test_data/parrot.jpg", 1) };
+	cv::Mat image{ cv::imread("C:/Admin/Programming/C++/OpenCV/data/test_data/parrot.jpg", 1) };
 	if (!image.data)
 		return -1;
 
-	first();
-	//second(image);
+	//grayAndColorHist(image);
+	lab2();
 
 	return 0;
 }
--- prj/Temp_prj/main.cpp
+++ prj/Temp_prj/main.cpp
@@ -1,11 +1,11 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
-#include <array>
+//#include <array>
 
-void tempTask()
+int main()
 {
 	// READ RGB color image and convert it to Lab
-	cv::Mat bgr_image = cv::imread("C:/Users/dimam/Documents/Admin/Programming/C++/OpenCV/OpenCVData/test_data/parrot.jpg");
+	cv::Mat bgr_image = cv::imread("C:/Admin/Programming/C++/OpenCV/data/test_data/parrot.jpg");
 	cv::Mat lab_image;
 	cv::cvtColor(bgr_image, lab_image, CV_BGR2Lab);
 
@@ -32,80 +32,6 @@ void tempTask()
 	cv::imshow("image CLAHE", image_clahe);
 	cv::waitKey();
 	cv::destroyAllWindows();
-}
-
-void second()
-{
-	cv::Mat image = cv::imread("C:/Users/dimam/Documents/Admin/Programming/C++/OpenCV/OpenCVData/test_data/parrot.jpg", CV_LOAD_IMAGE_COLOR);
-
-	// Преобразование в изображение в оттенках серого
-	cv::Mat imageGray;
-	cv::cvtColor(image, imageGray, CV_BGR2GRAY);
-
-	// Повышение яркости изображения
-	cv::Mat imageB = image + cv::Scalar(30, 30, 30);
-
-	cv::Mat imageGrayB;
-	cv::cvtColor(imageB, imageGrayB, CV_BGR2GRAY);
-
-	int histSize{ 256 };
-	float range[] = { 0,256 };
-	const float* histRange = { range };
-
-	bool uniform{ true };
-	bool accumulate{ false };
-
-	int hist_w{ 512 }; // 512
-	int hist_h{ 400 }; // 400
-	int bin_w{ cvRound((double)hist_w / histSize) };
-
-	// Создание пустых изображений для гистограмм
-	cv::Mat hist_window(hist_h, hist_w, CV_8UC3, cv::Scalar(255, 255, 255));
-	cv::Mat histBright_window(hist_h, hist_w, CV_8UC3, cv::Scalar(255, 255, 255));
-
-	// Рассчет гистограмм
-	cv::Mat hist;
-	cv::Mat hist_bright;
-	calcHist(&imageGray, 1, 0, cv::Mat(), hist, 1, &histSize, &histRange, uniform, accumulate);
-	calcHist(&imageGrayB, 1, 0, cv::Mat(), hist_bright, 1, &histSize, &histRange, uniform, accumulate);
-
-	// Нормализация гистограмм под размер окна
-	cv::normalize(hist, hist, 0, hist_window.rows, cv::NORM_MINMAX, -1, cv::Mat());
-	cv::normalize(hist_bright, hist_bright, 0, histBright_window.rows, cv::NORM_MINMAX, -1, cv::Mat());
-
-	// Построение гистограмм
-	for (int i = 1; i < histSize; ++i)
-	{
-		line(hist_window, cv::Point(bin_w * (i - 1), hist_h - cvRound(hist.at<float>(i - 1))),
-			cv::Point(bin_w * (i), hist_h - cvRound(hist.at<float>(i))),
-			cv::Scalar(0, 0, 0), 2, 8, 0);
-		line(histBright_window, cv::Point(bin_w * (i - 1), hist_h - cvRound(hist_bright.at<float>(i - 1))),
-			cv::Point(bin_w * (i), hist_h - cvRound(hist_bright.at<float>(i))),
-			cv::Scalar(0, 0, 0), 2, 8, 0);
-	}
-
-	// Отображение результатов
-
-	cv::namedWindow("Original image in gray", CV_WINDOW_AUTOSIZE);
-	cv::namedWindow("Hist", cv::WINDOW_AUTOSIZE);
-	cv::namedWindow("Bright image", cv::WINDOW_AUTOSIZE);
-	cv::namedWindow("Bright Hist", cv::WINDOW_AUTOSIZE);
-
-	cv::imshow("Original image in gray", imageGray);
-	cv::imshow("Hist", hist_window);
-	cv::imshow("Bright image", imageGrayB);
-	cv::imshow("Bright Hist", histBright_window);
-
-	cv::waitKey(0);
-
-	cv::destroyAllWindows();
-}
-
-
-int main()
-{
-	//tempTask();
-	//second();
 
 	return 0;
 }
