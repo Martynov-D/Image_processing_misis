07e82248341f3187a76ee3f0cec0068f6de70625
Martynov Dima <dimamartynov97@mail.ru>
2020-05-21 01:19:14 +0300

    Disparity map and algorithm description

--- prj/Semester_work/main.cpp
+++ prj/Semester_work/main.cpp
@@ -2,6 +2,8 @@
 #include "opencv2/calib3d/calib3d.hpp"
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <vector>
 
 cv::Mat getImage(std::string name)
 {
@@ -19,12 +21,12 @@ cv::Mat sgbm(const cv::Mat& left, const cv::Mat& right, int nDisparities, int SA
     int 	blockSize = SADWindowSize;
     int 	P1 = 8 * 1 * blockSize * blockSize;
     int 	P2 = 32 * 1 * blockSize * blockSize;
-    int 	disp12MaxDiff = 10;
+    int 	disp12MaxDiff = 15;
     int 	preFilterCap = 4;
-    int 	uniquenessRatio = 1;
-    int 	speckleWindowSize = 150;
-    int 	speckleRange = 2;
-    int 	mode = cv::StereoSGBM::MODE_SGBM;
+    int 	uniquenessRatio = 60;
+    int 	speckleWindowSize = 40; 
+    int 	speckleRange = 1;
+    int 	mode = cv::StereoSGBM::MODE_HH;
     cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(minDisparity, numDisparities, blockSize, P1, P2, disp12MaxDiff, preFilterCap, uniquenessRatio, speckleWindowSize, speckleRange, mode);
     cv::Mat disp;
     sgbm->compute(left, right, disp);
@@ -35,7 +37,7 @@ cv::Mat sgbm(const cv::Mat& left, const cv::Mat& right, int nDisparities, int SA
 cv::Mat bm(const cv::Mat& left, const cv::Mat& right, int nDisparities, int SADWindowSize)
 {
     cv::Ptr<cv::StereoBM> sbm = cv::StereoBM::create(nDisparities, SADWindowSize);
-    cv::Mat disp;
+    cv::Mat disp(left.rows, left.cols, CV_32FC1);
     sbm->compute(left, right, disp);
 
     return disp;
@@ -43,53 +45,62 @@ cv::Mat bm(const cv::Mat& left, const cv::Mat& right, int nDisparities, int SADW
 
 int main()
 {
-    /*std::string left_im{ "ambush_5_left.jpg" };
+   /* std::string left_im{ "ambush_5_left.jpg" };
     std::string right_im{ "ambush_5_right.jpg" };*/
 
    /* std::string left_im{ "left.png" };
     std::string right_im{ "right.png" };*/
 
-    std::string left_im{ "5_l.png" };
-    std::string right_im{ "5_r.png" };
+    std::string left_im{ "38_l.png" };
+    std::string right_im{ "38_r.png" };
+    
+    /*std::string left_im{ "inLeft.pgm" };
+    std::string right_im{ "inRight.pgm" };*/
 
     cv::Mat left = getImage(left_im);
     cv::Mat right = getImage(right_im);
 
-    int numDisparities = 16 * 4;
-    int SADWindowSize = 11;
+    int numDisparities = 16 * 3;
+    int SADWindowSize = 5;
 
     cv::Mat disp;
     cv::Mat disp8;
-    cv::Mat disp32f;
 
     if (false)
         disp = bm(left, right, numDisparities, SADWindowSize);
     else
         disp = sgbm(left, right, numDisparities, SADWindowSize);
+    cv::normalize(disp, disp8, 0, 255, cv::NORM_MINMAX, CV_8UC1);
     
-    /*  
+    /*
     depth = baseline * focal / disparity
     Для KITTI  baseline = 0.54m, фокусное расстояние ~721 pix.
     Диспаратность, которая получается на выходе, должна быть преобразована к размеру исходного изображения == 1242.
     */
-    disp.convertTo(disp32f, CV_32F);
-    cv::Mat depth(disp32f.cols, disp32f.rows, disp32f.type());
-
-    for (int i{ 0 }; i < depth.rows; ++i)
-    {
-        for (int j{ 0 }; j < depth.cols; ++j)
-            depth.at<float>(i, j) = 127;//disp32f.at<float>(i, j);
-    }
-
-    //cv::Mat depth = 0.54 * 721 / (1242 * disp);
-    cv::normalize(disp, disp8, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+    cv::Mat depth = 0.54 * 721 / disp;
     cv::normalize(depth, depth, 0, 255, cv::NORM_MINMAX, CV_8UC1);
-    
+
+    cv::Mat dst;
+    cv::Mat detected_edges;
+    cv::Canny(disp8, detected_edges, 50, 50 * 3, 3);
+    disp8.copyTo(dst, detected_edges);
+
+    cv::Mat disp8E;
+    cv::Mat filterD = (cv::Mat_<uchar>(3,3) << 1, 0, 0, 0, 1, 0, 0, 0, 1);
+    cv::Mat filterE = (cv::Mat_<uchar>(3,3) << 0, 0, 1, 0, 1, 0, 1, 0, 0);
+    cv::Mat filterC = (cv::Mat_<uchar>(3, 3) << 0, 1, 0, 1, 0, 1, 0, 1, 0);
+    /*cv::erode(disp8, disp8E, filterD);
+    cv::erode(disp8E, disp8E, filterE);*/
+    //cv::erode(depth, disp8E, filterC);
+
     cv::imshow("Disparity map", disp8);
     cv::imshow("Depth", depth);
+    cv::imshow("Canny", dst);
+    //cv::imshow("Erode", disp8E);
+  
 
-    //cv::imwrite("C:/Admin/Programming/C++/OpenCV/data/lab_data/bm.png", bmImage, { CV_IMWRITE_PNG_COMPRESSION, 0 });
-    //cv::imwrite("C:/Admin/Programming/C++/OpenCV/data/lab_data/sgbm.png", sgbmImage, { CV_IMWRITE_PNG_COMPRESSION, 0 });
+    //cv::imwrite("C:/Admin/Programming/C++/OpenCV/data/lab_data/disp8.png", disp8, { CV_IMWRITE_PNG_COMPRESSION, 0 });
+    //cv::imwrite("C:/Admin/Programming/C++/OpenCV/data/lab_data/invertedDisp.png", depth, { CV_IMWRITE_PNG_COMPRESSION, 0 });
 
     cv::waitKey(0);
     cv::destroyAllWindows();
