e67555ca3cfd44eecd3730ca244beaef3071cd65
Martynov Dima <dimamartynov97@mail.ru>
2020-05-23 02:47:22 +0300

    Check point

--- prj/Semester_work/main.cpp
+++ prj/Semester_work/main.cpp
@@ -2,8 +2,9 @@
 #include "opencv2/calib3d/calib3d.hpp"
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include "opencv2/ximgproc.hpp"
 #include <iostream>
-#include <vector>
+
 
 cv::Mat getImage(std::string name)
 {
@@ -14,33 +15,53 @@ cv::Mat getImage(std::string name)
     return imgGray;
 }
 
-cv::Mat sgbm(const cv::Mat& left, const cv::Mat& right, int nDisparities, int SADWindowSize)
+cv::Mat stereoBm(const cv::Mat& left, const cv::Mat& right, int nDisparities, int SADWindowSize, bool algo)
 {
-    int 	minDisparity = 0;
-    int 	numDisparities = nDisparities;
-    int 	blockSize = SADWindowSize;
-    int 	P1 = 8 * 1 * blockSize * blockSize;
-    int 	P2 = 32 * 1 * blockSize * blockSize;
-    int 	disp12MaxDiff = 0;
-    int 	preFilterCap = 0;
-    int 	uniquenessRatio = 0;
-    int 	speckleWindowSize = 0;
-    int 	speckleRange = 0;
-    int 	mode = cv::StereoSGBM::MODE_HH;
-    cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(minDisparity, numDisparities, blockSize, P1, P2, disp12MaxDiff, preFilterCap, uniquenessRatio, speckleWindowSize, speckleRange, mode);
-    cv::Mat disp(left.rows, left.cols, CV_32FC1);
-    sgbm->compute(left, right, disp);
+    cv::Mat left_disp(left.rows, left.cols, CV_32F);
+    cv::Mat right_disp(left.rows, left.cols, CV_32F);
+    cv::Mat disp(left.rows, left.cols, CV_32F);
+    cv::Ptr<cv::ximgproc::DisparityWLSFilter> wls_filter;
 
-    return disp;
-}
+    // StereoBM
+    if (algo)
+    {
+        cv::Ptr<cv::StereoBM> left_sbm = cv::StereoBM::create(nDisparities, SADWindowSize);
+        //wls_filter = cv::ximgproc::createDisparityWLSFilter(left_sbm);
+        //cv::Ptr<cv::StereoMatcher> right_sbm = cv::ximgproc::createRightMatcher(left_sbm);
+        left_sbm->compute(left, right, left_disp);
+        //right_sbm->compute(left, right, right_disp);
+    }
+    // StereoSGBM
+    else
+    {
+        int 	minDisparity = 0;
+        int 	numDisparities = nDisparities;
+        int 	blockSize = SADWindowSize;
+        int 	P1 = 8 * 3 * blockSize * blockSize;
+        int 	P2 = 32 * 3 * blockSize * blockSize;
+        int 	disp12MaxDiff = 0;
+        int 	preFilterCap = 63;
+        int 	uniquenessRatio = 0;
+        int 	speckleWindowSize = 0;
+        int 	speckleRange = 0;
+        int 	mode = cv::StereoSGBM::MODE_HH;
 
-cv::Mat bm(const cv::Mat& left, const cv::Mat& right, int nDisparities, int SADWindowSize)
-{
-    cv::Ptr<cv::StereoBM> sbm = cv::StereoBM::create(nDisparities, SADWindowSize);
-    cv::Mat disp(left.rows, left.cols, CV_32FC1);
-    sbm->compute(left, right, disp);
+        cv::Ptr<cv::StereoSGBM> left_sgbm = cv::StereoSGBM::create(minDisparity, numDisparities, blockSize);
+        left_sgbm->setP1(P1);
+        left_sgbm->setP2(P2);
+        left_sgbm->setPreFilterCap(preFilterCap);
+        left_sgbm->setMode(mode);
+        //wls_filter = cv::ximgproc::createDisparityWLSFilter(left_sgbm);
+        //cv::Ptr<cv::StereoMatcher> right_sgbm = cv::ximgproc::createRightMatcher(left_sgbm);
 
-    return disp;
+        left_sgbm->compute(left, right, left_disp);
+        //right_sgbm->compute(left, right, right_disp);
+    }
+    /*wls_filter->setLambda(8000);
+    wls_filter->setSigmaColor(1.5);
+    wls_filter->filter(left_disp, left, disp, right_disp);*/
+
+    return left_disp;
 }
 
 int main()
@@ -51,25 +72,28 @@ int main()
    /* std::string left_im{ "left.png" };
     std::string right_im{ "right.png" };*/
 
+    /*std::string left_im{ "inLeft.pgm" };
+   std::string right_im{ "inRight.pgm" };*/
+
     std::string left_im{ "38_l.png" };
     std::string right_im{ "38_r.png" };
-    
-    /*std::string left_im{ "inLeft.pgm" };
-    std::string right_im{ "inRight.pgm" };*/
-
+   
     cv::Mat left = getImage(left_im);
     cv::Mat right = getImage(right_im);
 
+   
     int numDisparities = 16 * 4;
-    int SADWindowSize = 9;
+    int SADWindowSize = 3;
+    bool mode = false;
+    if (mode)
+    {
+        SADWindowSize = 9;
+    }
 
     cv::Mat disp;
     cv::Mat disp8;
 
-    if (false)
-        disp = bm(left, right, numDisparities, SADWindowSize);
-    else
-        disp = sgbm(left, right, numDisparities, SADWindowSize);
+    disp = stereoBm(left, right, numDisparities, SADWindowSize, mode);
     cv::normalize(disp, disp8, 0, 255, cv::NORM_MINMAX, CV_8UC1);
 
     /*
@@ -77,13 +101,13 @@ int main()
     Для KITTI  baseline = 0.54m, фокусное расстояние ~721 pix.
     Диспаратность, которая получается на выходе, должна быть преобразована к размеру исходного изображения == 1242.
     */
-    /*cv::Mat depth = 721 * 0.54 / disp8;
-    cv::normalize(depth, depth, 0, 255, cv::NORM_MINMAX, CV_8UC1);*/
+    cv::Mat depth = 721 * 0.54 / disp8;
+    cv::normalize(depth, depth, 0, 255, cv::NORM_MINMAX, CV_8UC1);
 
-    /*cv::Mat dst;
+   /* cv::Mat canny;
     cv::Mat detected_edges;
     cv::Canny(disp8, detected_edges, 50, 50 * 3, 3);
-    disp8.copyTo(dst, detected_edges);*/
+    disp8.copyTo(canny, detected_edges);*/
 
    /* cv::Mat disp8E;
     cv::Mat filterD = (cv::Mat_<uchar>(3, 3) << 1, 0, 0, 0, 1, 0, 0, 0, 1);
@@ -94,18 +118,34 @@ int main()
     cv::Mat filterC = (cv::Mat_<uchar>(3, 3) << 0, 1, 0, 1, 0, 1, 0, 1, 0);
     cv::dilate(depth, depthE, filterC);*/
 
-    cv::Mat bgr;
-    cv::applyColorMap(disp8, bgr, cv::COLORMAP_JET);
+    cv::Mat bgrDisp;
+    cv::Mat bgrDepth;
+    cv::applyColorMap(disp8, bgrDisp, cv::COLORMAP_JET);
+    cv::applyColorMap(depth, bgrDepth, cv::COLORMAP_JET);
 
     cv::imshow("Disparity map", disp8);
-    //cv::imshow("Depth", depth);
-    cv::imshow("Colored", bgr);
-    //cv::imshow("Canny", dst);
+    cv::imshow("Depth", depth);
+    cv::imshow("Colored disp", bgrDisp);
+    cv::imshow("Colored depth", bgrDepth);
+    
+    //cv::imshow("Canny", canny);
     //cv::imshow("Erode", depthE);
   
+    if (mode)
+    {
+        cv::imwrite("C:/Admin/Programming/C++/OpenCV/data/lab_data/disp8_BM.png", disp8, { CV_IMWRITE_PNG_COMPRESSION, 0 });
+        cv::imwrite("C:/Admin/Programming/C++/OpenCV/data/lab_data/depth_BM.png", depth, { CV_IMWRITE_PNG_COMPRESSION, 0 });
+        cv::imwrite("C:/Admin/Programming/C++/OpenCV/data/lab_data/disp8_BGR_BM.png", bgrDisp, { CV_IMWRITE_PNG_COMPRESSION, 0 });
+        cv::imwrite("C:/Admin/Programming/C++/OpenCV/data/lab_data/depth_BGR_BM.png", bgrDepth, { CV_IMWRITE_PNG_COMPRESSION, 0 });
+    }
+    else
+    {
+        cv::imwrite("C:/Admin/Programming/C++/OpenCV/data/lab_data/disp8_SGBM.png", disp8, { CV_IMWRITE_PNG_COMPRESSION, 0 });
+        cv::imwrite("C:/Admin/Programming/C++/OpenCV/data/lab_data/depth_SGBM.png", depth, { CV_IMWRITE_PNG_COMPRESSION, 0 });
+        cv::imwrite("C:/Admin/Programming/C++/OpenCV/data/lab_data/disp8_BGR_SGBM.png", bgrDisp, { CV_IMWRITE_PNG_COMPRESSION, 0 });
+        cv::imwrite("C:/Admin/Programming/C++/OpenCV/data/lab_data/depth_BGR_SGBM.png", bgrDepth, { CV_IMWRITE_PNG_COMPRESSION, 0 });
 
-    //cv::imwrite("C:/Admin/Programming/C++/OpenCV/data/lab_data/disp8.png", disp8, { CV_IMWRITE_PNG_COMPRESSION, 0 });
-    //cv::imwrite("C:/Admin/Programming/C++/OpenCV/data/lab_data/invertedDisp.png", depth, { CV_IMWRITE_PNG_COMPRESSION, 0 });
+    }
 
     cv::waitKey(0);
     cv::destroyAllWindows();
